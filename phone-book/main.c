6dc12d8dd4fdc760423b39ebddfa769563c85096
harman-1411 <harmansiphone16@gmail.com>
2026-06-12 15:01:20 +0530

    Add files via upload

---
+++ phone-book/main.c
@@ -0,0 +1,246 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+char n;
+int a ;
+int p;
+int d;
+char add;
+
+#include <stdio.h>
+
+int main()
+{
+char na;
+int ag ;
+int ph;
+int da;
+char addr;
+
+printf("welcome to phone book\n\n");
+ 
+ 
+ printf("MENU\n");
+ 
+ 
+ printf("press 1 to add the contact\n\n");
+ 
+ int x ;
+ printf("enter\n");
+ scanf("%d",&x);
+ 
+ if(x==1)
+ {
+     
+     int y;
+     
+     printf("enter the number of contact to be added: ");
+     scanf("%d",&y);
+     
+     if(y==1)
+     {
+          printf("---------------\n");
+         printf("name :");
+         scanf("%s", &n);
+         printf("age : ");
+         scanf("%d",&a);
+         printf("phone number : ");
+         scanf("%d",&p);
+         printf("date of birth : ");
+         scanf("%d",&d);
+         printf("adress: ");
+         scanf("%s",&add );
+         printf("---------------\n");
+     }
+     else if(y==2)
+     {
+         
+         
+         
+         printf("age : ");
+         scanf("%d",&ag);
+         printf("---------------\n");
+         printf("name :");
+         scanf("%s",&n);
+         printf("age : ");
+         scanf("%d",&a);
+         printf("phone number : ");
+         scanf("%d",&p);
+         printf("date of birth : ");
+         scanf("%d",&d);
+         printf("adress: ");
+         scanf("%s",&add);
+         printf("---------------\n");
+     
+     
+     
+         printf("---------------\n");
+         printf("name :");
+         scanf("%s",&na);
+         scanf("%d",&ph);
+         printf("date of birth : ");
+         scanf("%d",&da);
+         printf("adress: ");
+         scanf("%s",&addr);
+         printf("---------------\n");
+     }
+     
+     printf("press 2 to see the entered contact\n");
+     printf("press 3 to exist the phone book\n");
+     
+     int z;
+     printf("enter\n");
+     scanf("%d",&z);
+     
+     if(z==2)
+     {
+         int num;
+         printf("enter the number of contact to be viewed : ");
+         scanf("%d",&num);
+         
+         if(num==1)
+         {
+              printf("---------------\n");
+              printf("name :");
+              printf("%s",&n);
+              printf("age : ");
+              scanf("%d",&ag);
+              printf("phone number : ");
+              scanf("%d",&p);
+              printf("date of birth : ");
+              scanf("%d",&d);
+              printf("adress: ");
+              scanf("%s",&add);
+              printf("---------------\n");
+         }
+         else if (num==2)
+         
+         {
+             printf("age : ");
+            scanf("%d",&ag);
+            printf("---------------\n");
+            printf("name :");
+            printf("%s",&n);
+            printf("age : ");
+           scanf("%d",&a);
+           printf("phone number : ");
+           scanf("%d",&p);
+           printf("date of birth : ");
+           scanf("%d",&d);
+           printf("adress: ");
+           printf("%s",&add);
+           printf("---------------\n");
+     
+     
+     
+           printf("---------------\n");
+           printf("name :");
+           scanf("%s",&na  );
+           scanf("%d",&ph);
+           printf("date of birth : ");
+           scanf("%d",&da);
+           printf("adress: ");
+           printf("%s",&addr);
+           printf("---------------\n");
+           
+         }
+         
+     
+     
+     else if(z==3)
+     {
+     printf("are you sure you want to close the phone book\n");
+     printf("press 1 for yess\n");
+     printf("press 2 for no\n");
+     
+     
+    int ans;
+    printf("enter\n");
+    scanf("%d",&ans);
+    
+    
+    if(ans==1)
+    
+    {
+        
+        printf("the phone book has closed\n");
+        printf("have a good day\n");
+    }
+    else if (ans==2)
+    {
+        printf("the phone book has not closed yet\n");
+        printf("you can contact\n");
+    }
+    else 
+    printf("not valid");
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+    
+     
+ 
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+    
+    
+        
